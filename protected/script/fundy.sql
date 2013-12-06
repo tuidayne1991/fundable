fb06b4790d04bb226700d7b00ce22c5a89cfb394
Thanh <ctt.bk.hcmut2009@gmail.com>
2013-12-06 11:40:37 +0700

    add script

--- protected/script/fundy.sql
+++ protected/script/fundy.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.9
+-- version 4.0.4.1
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 03, 2013 at 06:11 AM
--- Server version: 5.5.34-0ubuntu0.12.04.1
--- PHP Version: 5.3.10-1ubuntu3.8
+-- Generation Time: Dec 06, 2013 at 05:39 AM
+-- Server version: 5.6.12
+-- PHP Version: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `fundy`
 --
+CREATE DATABASE IF NOT EXISTS `fundy` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `fundy`;
 
 -- --------------------------------------------------------
 
@@ -33,14 +35,19 @@ CREATE TABLE IF NOT EXISTS `fundy_user` (
   `is_activated` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `fundy_user`
 --
 
 INSERT INTO `fundy_user` (`id`, `email`, `password`, `is_activated`) VALUES
-(1, 'tuidayne1991@gmail.com', 'e99a18c428cb38d5f260853678922e03', 1);
+(1, 'tuidayne1991@gmail.com', 'e99a18c428cb38d5f260853678922e03', 1),
+(2, 'ctt.bk.hcmut2009@gmail.com', 'abc123', 0),
+(3, 'tonycaovn@gmail.com', 'abc123', 0),
+(4, 'helloworld@gmail.com', 'abc123', 0),
+(5, 'abc@gmail.com', 'abc123', 0),
+(6, 'abc3@gmail.com', 'abc123', 0);
 
 -- --------------------------------------------------------
 
@@ -51,45 +58,33 @@ INSERT INTO `fundy_user` (`id`, `email`, `password`, `is_activated`) VALUES
 CREATE TABLE IF NOT EXISTS `money_box` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) NOT NULL,
+  `source` text COLLATE utf8_unicode_ci NOT NULL,
   `balance` double NOT NULL,
   `capacity` double NOT NULL,
   `currency` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   KEY `owner_id_3` (`owner_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=13 ;
-
---
--- Dumping data for table `money_box`
---
-
-INSERT INTO `money_box` (`id`, `owner_id`, `balance`, `capacity`, `currency`) VALUES
-(5, 1, 4234, 3123, 'asda'),
-(6, 1, 1312, 123124, '1231sasa'),
-(7, 1, 4234, 123214, 'dasdas'),
-(8, 1, 100, 50, 'sfsdf'),
-(9, 1, 500, 10, 'sdfsdfs'),
-(10, 1, 4234, 12321, 'd23d23d'),
-(11, 1, 4234, 1, 'addas'),
-(12, 1, 0, 543, 'fvdsvsd');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=20 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `transaction`
+-- Table structure for table `Transaction`
 --
 
-CREATE TABLE IF NOT EXISTS `transaction` (
+CREATE TABLE IF NOT EXISTS `Transaction` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `owner_id` int(11) NOT NULL,
   `box_id` int(11) NOT NULL,
   `money` double NOT NULL,
   `type` tinyint(4) DEFAULT NULL,
   `description` text COLLATE utf8_unicode_ci,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `currency` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `owner_id` (`owner_id`),
-  UNIQUE KEY `box_id` (`box_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  KEY `owner_id` (`owner_id`),
+  KEY `box_id` (`box_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=20 ;
 
 --
 -- Constraints for dumped tables
@@ -102,11 +97,10 @@ ALTER TABLE `money_box`
   ADD CONSTRAINT `money_box_user_constraint` FOREIGN KEY (`owner_id`) REFERENCES `fundy_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Constraints for table `transaction`
+-- Constraints for table `Transaction`
 --
-ALTER TABLE `transaction`
-  ADD CONSTRAINT `transaction_box_constraint` FOREIGN KEY (`box_id`) REFERENCES `money_box` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `transaction_user_constraint` FOREIGN KEY (`owner_id`) REFERENCES `fundy_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `Transaction`
+  ADD CONSTRAINT `transaction_ibfk_1` FOREIGN KEY (`owner_id`) REFERENCES `fundy_user` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
