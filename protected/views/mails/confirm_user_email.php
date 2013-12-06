a00667bc9d9d97b33d79ec81da8de694b66e7780
Thanh <ctt.bk.hcmut2009@gmail.com>
2013-12-07 01:29:40 +0700

    Still bugging

--- protected/views/mails/confirm_user_email.php
+++ protected/views/mails/confirm_user_email.php
@@ -1,6 +1,1 @@
-<? 
-    $url = $this->createAbsoluteUrl('user/confirm', array('key' => $recipient->unique_id , 'ac' => $recipient->activation_code));
-?>
-Hi, please click following link to confirm your email<br/>
-<a href="<?= $url?>"><?= $url ?></a>
-
+HEllo World
